492483709e61f205c4a10961ac12b781a444512b
sapna <diintechteam4@gmailcom>
2025-09-23 19:10:09 +0530

    update

--- routes/cart.js
+++ routes/cart.js
@@ -3,17 +3,15 @@ const router = express.Router({ mergeParams: true });
 const { authenticateMobileUser } = require('../middleware/mobileAuth');
 const cartController = require('../controllers/cartController');
 
-router.post('/callback',cartController.paytmCallback)
 // All routes require mobile authentication and client in path
-router.use(authenticateMobileUser);
-
-router.get('/', cartController.getCart);
-router.post('/add', cartController.addItem);
-router.post('/update', cartController.updateItem);
-router.delete('/item/:workbookId', cartController.removeItem);
-router.post('/clear', cartController.clearCart);
+router.get('/', authenticateMobileUser, cartController.getCart);
+router.post('/add', authenticateMobileUser, cartController.addItem);
+router.post('/update', authenticateMobileUser, cartController.updateItem);
+router.delete('/item/:workbookId', authenticateMobileUser, cartController.removeItem);
+router.post('/clear', authenticateMobileUser, cartController.clearCart);
 // Checkout flows
-router.post('/checkout/item', cartController.checkoutItem);
+router.post('/checkout/item', authenticateMobileUser, cartController.checkoutItem);
+router.post('/callback',cartController.paytmCallback)
 
 module.exports = router;
 
